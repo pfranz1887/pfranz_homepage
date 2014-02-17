6fc9cd03be5aece4dae77fac4ef0b0ea6fff31b5
Philip Franz <pfranz@smiac.speedport_w723_v_typ_a_1_00_098>
2014-02-17 10:56:31 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PfranzHomepage::Application.config.secret_key_base = '52bf98bc1670273c32cfa28cc7e341deddd46485cc8d26f5231581ac7dd12e6c929cc84260f671858659647214e5ddfada2d27414a5e00d14d46864ff01ff4c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PfranzHomepage::Application.config.session_store :cookie_store, key: '_pfranz_homepage_session'
